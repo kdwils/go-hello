9f74c12ef9bece7a2e13e1a0177d6d9b559ca3dc
Kyle Wilson <wilson15701@gmail.com>
2024-05-28 11:50:08 -0400

    add deploy for argo

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/kyledwilson/go-hello
 
-go 1.15
+go 1.22
--- main.go
+++ main.go
@@ -6,10 +6,15 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/", HelloHandler)
+	http.HandleFunc("/canary", RolloutHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
+
+func RolloutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from the preview version, %s!", r.URL.Path[1:])
 }
