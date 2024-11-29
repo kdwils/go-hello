027d2db729c73dc30024c5c14032e856483a1db8
Kyle Wilson <wilson15701@gmail.com>
2024-11-29 14:59:27 -0500

    add healthz

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/healthz", Healthz)
 	http.HandleFunc("/stable/", HelloHandler)
 	// http.HandleFunc("/rollout/", RolloutHandler)
 	http.ListenAndServe(":8080", nil)
@@ -15,6 +16,6 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }
 
-func RolloutHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from the current preview version, %s!", r.URL.Path[1:])
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "ok")
 }
